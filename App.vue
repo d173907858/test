8dcf1c4e8c4062e2340214fe2efe7038d60622b7
d173907858 <173907858@qq.com>
2018-02-27 12:25:06 +0800

    first

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+   <app-header></app-header>
+    
+  </div>
+</template>
+
+<script>
+
+import AppHeader from './components/containers/Header/Header'
+
+export default {
+  name:'App',
+  components: {
+   AppHeader
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+@import './stylesheets/_reset.scss';
+@import './stylesheets/_common.scss';
+</style>
